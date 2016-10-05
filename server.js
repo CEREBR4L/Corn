61595bac58da99c1ef9f1914bbf12086ffd9c51b
CEREBR4L <mattmillergraphics@gmail.com>
2016-10-05 19:18:03 +0100

    Now using the default mongo port..

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27018/database');
+mongoose.connect('mongodb://localhost:27017/database');
 
 var backend = require('./backend/app.js');
 
